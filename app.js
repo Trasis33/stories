7b3f45eb8d8a18de887b6ace44ece6d29af47197
Fredrik Langå <fl222pw@student.lnu.se>
2020-06-21 17:07:39 +0200

    implement routes

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // eslint-disable-line no-unused-vars
+const morgan = require('morgan')
+const hbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 connectDB()
 
 const app = express()
+
+// logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// template engine
+app.engine('.hbs', hbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+// routes
+app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
 
