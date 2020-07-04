57b290bc1e7971c4a9a3cd67875cb631145ce3e2
Fredrik Langå <fl222pw@student.lnu.se>
2020-07-04 20:48:15 +0200

    implement edit and delete functionality

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config({ path: './config/config.env' }) // eslint-disable-line no-unused-vars
 const morgan = require('morgan')
 const hbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const SessionStore = require('connect-mongo')(session)
@@ -19,17 +20,28 @@ const app = express()
 
 app.use(express.urlencoded({ extended: false }))
 
+// method override for PUT
+app.use(
+  methodOverride((req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 // logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-const { formatDate, truncate, stripTags } = require('./helpers/hbs')
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs')
 
 // template engine
 app.engine(
   '.hbs',
-  hbs({ helpers: { formatDate, truncate, stripTags }, defaultLayout: 'main', extname: '.hbs' })
+  hbs({ helpers: { formatDate, truncate, stripTags, editIcon, select }, defaultLayout: 'main', extname: '.hbs' })
 )
 app.set('view engine', '.hbs')
 
@@ -46,6 +58,12 @@ app.use(
 // passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// global var for access to logged in user in templates
+app.use((req, res, next) => {
+  res.locals.user = req.user || null
+  next()
+})
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')))
